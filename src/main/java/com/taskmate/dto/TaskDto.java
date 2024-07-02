596f5eea44d383601736add9203e4cc62784ccca
MYSELFARPITT <arpitmehandiratta8@gmail.com>
2024-07-03 02:29:27 +0530

    first commit

---
+++ src/main/java/com/taskmate/dto/TaskDto.java
@@ -0,0 +1,22 @@
+package com.taskmate.dto;
+
+import java.util.Date;
+
+import com.taskmate.enums.TaskStatus;
+
+import lombok.Data;
+
+
+@Data
+public class TaskDto {
+	
+	private Long id;
+	
+	private String title;
+	private String description;
+	private Date dueDate;
+	private String priority;
+	private TaskStatus taskStatus;
+	private Long employeeId;
+	private String employeeName;
+}
---
+++ src/main/java/com/taskmate/services/admin/AdminService.java
@@ -0,0 +1,32 @@
+package com.taskmate.services.admin;
+
+import java.util.List;
+
+import com.taskmate.dto.CommentDTO;
+import com.taskmate.dto.TaskDto;
+import com.taskmate.dto.UserDto;
+
+public interface AdminService {
+	
+	List<UserDto> getUsers();
+	TaskDto  createTask(TaskDto taskDto);
+	
+	List<TaskDto> getAllTasks();
+	
+    void deleteTask(Long id);   
+   
+    
+    TaskDto updateTask(Long id , TaskDto taskDto) ;
+    
+    
+    
+    
+    List<TaskDto> searchTaskByTitle(String title);
+    
+    TaskDto getTaskById(Long id);
+    
+    CommentDTO createComment(Long taskId, String content);
+    
+   List<CommentDTO> getCommentsByTaskId(Long taskId);
+   	 
+}
